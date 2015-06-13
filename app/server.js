98f975ca3405939330be3747b5513694a8aa0e13
Christoffer Åhrling <christoffer@nilpath.se>
2015-06-13 21:22:47 +0200

    refactored the server a bit

--- app/server.js
+++ app/server.js
@@ -8,33 +8,77 @@ var bodyParser = require('body-parser');
 
 require('node-jsx').install();
 
+var TodoApp = react.createFactory(require('./public/js/components/TodoApp.js'));
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var TodoApp = react.createFactory(require('./public/js/components/TodoApp.js'));
-
-app.get('/', function(req, res) {
-  
+function getTasks(cb) {
+  if(!cb) { return; }
+    
   request
     .get('http://localhost:5000/api/tasks')
-    .end(function(err, taskRes) {
-      
-      var html = react.renderToString(
-        TodoApp({
-          tasks: taskRes.body
-        })
-      );
-      
-      res.render('index.ejs', {
-        app: html,
-        state: JSON.stringify(taskRes.body)
-      });
+    .end(function(err, res) {
+      cb(res.body);
     });
+    
+}
+
+function newTask(task, cb) {
+  if(!task) { return; }
+  
+  request
+    .post('http://localhost:5000/api/tasks')
+    .send(task)
+    .end(function(error){
+      if(error) {
+        console.log('failed to save new task: ', error);
+      }
+      if(cb) {
+        cb();
+      }
+    });
+}
+
+function updateTask(task, cb) {
+  if(!task) { return; }
+  
+  var url = ['http://localhost:5000/api/tasks/', task._id].join('');
+  
+  request
+    .put(url)
+    .send(task)
+    .end(function (error) {
+      if(error) {
+        console.log('failed to update task: ', error);
+      }
+      if(cb) {
+        cb();
+      }
+    });
+}
+
+// routes
+app.get('/', function(req, res) {
+  
+  getTasks(function (tasks) {
+    var html = react.renderToString(
+      TodoApp({
+        tasks: tasks
+      })
+    );
+    
+    res.render('index.ejs', {
+      app: html,
+      state: JSON.stringify(tasks)
+    });
+  });
   
 });
 
@@ -42,15 +86,9 @@ app.post('/tasks/new', function(req, res) {
   var description = req.body.description;
   
   if(description) {
-    request
-      .post('http://localhost:5000/api/tasks')
-      .send({description: description})
-      .end(function(error){
-        if(error) {
-          console.log('failed to save new task: ', error);
-        }
-        res.redirect('/');
-      });
+    newTask({description: description}, function() {
+      res.redirect('/');
+    });
   }
   
 });
@@ -60,53 +98,30 @@ app.post('/tasks/:id/toggle', function (req, res) {
   var done = !!req.body.done;
   var order = Number(req.body.order);
   var description = req.body.description;
-  var url = ['http://localhost:5000/api/tasks/', id].join('');
+  var task = {_id: id, description: description, order: order, done: done};
   
   if(id) {
-    request
-      .put(url)
-      .send({done: done, description: description, order: order})
-      .end(function (error) {
-        if(error) {
-          console.log('failed to update task: ', error);
-        }
-        res.redirect('/');
-      });
+    updateTask(task, function () {
+      res.redirect('/');
+    });
   }
 });
 
 app.post('/tasks/allDone', function (req, res) {
   
-  request
-    .get('http://localhost:5000/api/tasks')
-    .end(function(err, taskRes) {
-      var tasks = taskRes.body;
-      
-      if(tasks) {
-        var undone = tasks.filter(function (task) {
-          return !tasks.done;
-        });
-        
-        undone.forEach(function (task) {
-          var id = task._id;
-          var url = ['http://localhost:5000/api/tasks/', id].join('');
-          task.done = true;
-          
-          request
-            .put(url)
-            .send(task)
-            .end(function (error) {
-              if(error) {
-                console.log('failed to update task: ', error);
-              }
-            });
-          
-        });
-        
-        res.redirect('/');
-      }
-      
+  getTasks(function (tasks) {
+    var undone = tasks.filter(function (task) {
+      return !tasks.done;
     });
+    
+    undone.forEach(function (task) {
+      task.done = true;
+      updateTask(task);
+    });
+    
+    res.redirect('/');
+    
+  });
   
 });
 
